a6de83b28d18c98b010f4c060d47c923488bcc7f
oleksandr <matsibora.alexander@gmail.com>
2024-11-26 15:14:38 +0200

    SEO, icons

--- assets/vue-i18n.runtime.9ebc3dcb.js
+++ assets/vue-i18n.runtime.9ebc3dcb.js
@@ -1,4 +1,4 @@
-import{d as dt,h as zt,e as An,i as Sn,o as Pn,a as yn,c as Oe,w as st,F as en,g as tn,f as Rn,r as Dn,s as kn,j as Mn,T as Fn}from"./index.8a4d6802.js";/*!
+import{d as dt,h as zt,e as An,i as Sn,o as Pn,a as yn,c as Oe,w as st,F as en,g as tn,f as Rn,r as Dn,s as kn,j as Mn,T as Fn}from"./index.bf8f6945.js";/*!
   * shared v9.14.1
   * (c) 2024 kazuya kawaguchi
   * Released under the MIT License.
